24c904371950264e9e2500beae84e3d9837a71e1
iqubik <iqubik@gmail.com>
2026-04-12 23:35:10 +0300

    Update phpinfo.php

--- data/phpinfo.php
+++ data/phpinfo.php
@@ -1,3 +1,5 @@
 <?php
-// file: phpinfo.php v1.0
-phpinfo();
+// file: phpinfo.php v1.1
+// Ограниченный вывод: только общая информация и конфиг
+// Без переменных окружения, $_SERVER, $_GET и других чувствительных данных
+phpinfo(INFO_GENERAL | INFO_CONFIGURATION | INFO_MODULES);
